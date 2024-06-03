714440eafac7d8f63f7d26084d21a27a5519a991
jainamit130 <jainamit130@gmail.com>
2024-06-03 11:31:42 +0530

    Small Changes

--- src/environment.ts
+++ src/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: false,
     // baseUrl: 'http://reddit-clone.ap-south-1.elasticbeanstalk.com/reddit/'
-    // baseUrl: 'https://reddit-clone-backend-otzv.onrender.com/reddit/'
-    baseUrl: 'http://localhost:8080/reddit/'
+    baseUrl: 'https://reddit-clone-backend-otzv.onrender.com/reddit/'
+    // baseUrl: 'http://localhost:8080/reddit/'
   };
