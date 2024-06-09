2c928aa384f8be908e37a1f4578d808c86d232f1
jainamit130 <jainamit130@gmail.com>
2024-06-09 12:35:30 +0530

    Changes in login and signup screen size and the toggle button

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,6 +8,7 @@ import { HomeComponent } from './home/home.component';
 import { CommunitiesComponent } from './communities/communities.component';
 import { CreatePostComponent } from './create-post/create-post.component';
 import { SearchResultNavigationComponent } from './search-results/search-result-navigation.ts/search-result-navigation.component';
+import { UserService } from './shared/user.service';
 
 @Component({
   selector: 'app-root',
@@ -20,13 +21,19 @@ export class AppComponent implements OnInit{
 
   isLoginPage:boolean=false;
   settingsOn: boolean = false;
+  communityToggle:boolean = true;
 
-  constructor(private router: Router) {}
+  constructor(private router: Router,private userService:UserService) {}
 
   ngOnInit(): void {
     this.router.events.subscribe(event => {
       if (event instanceof NavigationEnd) {
         this.isLoginPage = this.router.url === '/login' || this.router.url === '/signup';
+        if(this.isLoginPage){
+          this.communityToggle=false;
+        } else {
+          this.communityToggle=true;
+        }
       }
     });
   }
