dc7b31d11b2cf99aaf4fa3309864b5b12783d7ff
Amit Choraria <jainamit130@gmail.com>
2024-07-09 16:43:10 +0530

    Changed Deployer Platform to GCP

--- src/environment.ts
+++ src/environment.ts
@@ -1,6 +1,7 @@
 export const environment = {
     production: false,
     // baseUrl: 'http://reddit-clone.ap-south-1.elasticbeanstalk.com/reddit/'
-    baseUrl: 'https://reddit-clone-backend-otzv.onrender.com/reddit/'
+    // baseUrl: 'https://reddit-clone-backend-otzv.onrender.com/reddit/'
+    baseUrl: 'https://reddi-clone-428708.el.r.appspot.com/reddit/'
     // baseUrl: 'http://localhost:8080/reddit/'
   };
